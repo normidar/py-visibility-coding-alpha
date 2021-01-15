d9eb0f70cabf83a4a46bf5286c07df91ea2871cf
劉達 <ryu@anchorz.co.jp>
2021-01-16 00:09:58 +0900

    u

--- app/saver/Saver.py
+++ app/saver/Saver.py
@@ -1,13 +1,23 @@
+import yaml
 from app.engine.NoReturnFig.Project import Project
 
 
 class Saver(object):
-    
+    # 从文件翻译到实例
     @staticmethod
     def from_file_to_object(path: str):
-        a = Saver.new_instance("Project",**{"a":"b"})
+        obj = Saver.from_yaml_to_map(path)
+        for i in obj.keys():
+            a = Saver.new_instance(i,**obj[i])
         print(a.a)
         print(a.__class__.__name__)
+
+    # 从yaml翻译到
+    @staticmethod
+    def from_yaml_to_map(path: str):
+        with open(path) as f:
+            obj = yaml.load(f)
+            return obj
 
     @staticmethod
     def new_instance(cls_name,**kwargs):
@@ -15,8 +25,18 @@ class Saver(object):
         instance.__dict__ = kwargs
         return instance
     
+    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+    @staticmethod
+    def from_map_to_yaml(path: str,obj):
+        with open(path, "w") as f:
+            yaml.dump(obj, f)
     @staticmethod
     def run():
-        Saver.from_file_to_object("")
+        pass 
+        # Saver.from_file_to_object("input.yaml")
+        # x = Saver.from_yaml_to_map("input.yaml")
+        # print(x)
+        # Saver.from_map_to_yaml("output.yaml",{"project":{"cls_name":"Project","codes":[{"cls0":{}}]}})
 
 Saver.run()
